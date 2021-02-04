c4bfe34b7a1805c09b6d3157d5adf8391ad2afc0
Jenny Paul <jenny@LAPTOP-PQ2SB0U6>
2021-02-04 11:52:00 -0600

    fixing the text style in the drop downs

--- JennyCasey_Assign2/Form1.Designer.cs
+++ JennyCasey_Assign2/Form1.Designer.cs
@@ -48,16 +48,16 @@ namespace JennyCasey_Assign2
             this.DisbandGuildButton = new System.Windows.Forms.Button();
             this.GuildRosterButton = new System.Windows.Forms.Button();
             this.CreatePlayerBox = new System.Windows.Forms.GroupBox();
-            this.CreateGuildBox = new System.Windows.Forms.GroupBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.Name_Textbox = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.Class_Dropdown = new System.Windows.Forms.ComboBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.Race_Dropdown = new System.Windows.Forms.ComboBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.Role_Dropdown = new System.Windows.Forms.ComboBox();
             this.AddPlayer_Button = new System.Windows.Forms.Button();
+            this.Role_Dropdown = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Race_Dropdown = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Class_Dropdown = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Name_Textbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CreateGuildBox = new System.Windows.Forms.GroupBox();
             this.ManagementFuncBox.SuspendLayout();
             this.CreatePlayerBox.SuspendLayout();
             this.SuspendLayout();
@@ -284,93 +284,6 @@ namespace JennyCasey_Assign2
             this.CreatePlayerBox.TabStop = false;
             this.CreatePlayerBox.Text = "Create New Player";
             // 
-            // CreateGuildBox
-            // 
-            this.CreateGuildBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CreateGuildBox.Location = new System.Drawing.Point(12, 348);
-            this.CreateGuildBox.Name = "CreateGuildBox";
-            this.CreateGuildBox.Size = new System.Drawing.Size(633, 139);
-            this.CreateGuildBox.TabIndex = 9;
-            this.CreateGuildBox.TabStop = false;
-            this.CreateGuildBox.Text = "Create New Guild";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(10, 23);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(93, 17);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Player Name ";
-            // 
-            // Name_Textbox
-            // 
-            this.Name_Textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Name_Textbox.Location = new System.Drawing.Point(13, 43);
-            this.Name_Textbox.Name = "Name_Textbox";
-            this.Name_Textbox.Size = new System.Drawing.Size(192, 27);
-            this.Name_Textbox.TabIndex = 10;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(10, 73);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(42, 17);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Class";
-            // 
-            // Class_Dropdown
-            // 
-            this.Class_Dropdown.FormattingEnabled = true;
-            this.Class_Dropdown.Location = new System.Drawing.Point(13, 93);
-            this.Class_Dropdown.Name = "Class_Dropdown";
-            this.Class_Dropdown.Size = new System.Drawing.Size(192, 28);
-            this.Class_Dropdown.TabIndex = 12;
-            this.Class_Dropdown.SelectedValueChanged += new System.EventHandler(this.Class_Dropdown_SelectedValueChanged);
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.Black;
-            this.label7.Location = new System.Drawing.Point(248, 23);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(41, 17);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Race";
-            // 
-            // Race_Dropdown
-            // 
-            this.Race_Dropdown.FormattingEnabled = true;
-            this.Race_Dropdown.Location = new System.Drawing.Point(251, 43);
-            this.Race_Dropdown.Name = "Race_Dropdown";
-            this.Race_Dropdown.Size = new System.Drawing.Size(192, 28);
-            this.Race_Dropdown.TabIndex = 14;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.Black;
-            this.label8.Location = new System.Drawing.Point(248, 74);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(37, 17);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "Role";
-            // 
-            // Role_Dropdown
-            // 
-            this.Role_Dropdown.FormattingEnabled = true;
-            this.Role_Dropdown.Location = new System.Drawing.Point(251, 93);
-            this.Role_Dropdown.Name = "Role_Dropdown";
-            this.Role_Dropdown.Size = new System.Drawing.Size(192, 28);
-            this.Role_Dropdown.TabIndex = 16;
-            // 
             // AddPlayer_Button
             // 
             this.AddPlayer_Button.BackColor = System.Drawing.SystemColors.Control;
@@ -383,6 +296,96 @@ namespace JennyCasey_Assign2
             this.AddPlayer_Button.Text = "Add Player";
             this.AddPlayer_Button.UseVisualStyleBackColor = false;
             this.AddPlayer_Button.Click += new System.EventHandler(this.AddPlayer_Button_Click);
+            // 
+            // Role_Dropdown
+            // 
+            this.Role_Dropdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Role_Dropdown.FormattingEnabled = true;
+            this.Role_Dropdown.Location = new System.Drawing.Point(251, 93);
+            this.Role_Dropdown.Name = "Role_Dropdown";
+            this.Role_Dropdown.Size = new System.Drawing.Size(192, 28);
+            this.Role_Dropdown.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(248, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Role";
+            // 
+            // Race_Dropdown
+            // 
+            this.Race_Dropdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Race_Dropdown.FormattingEnabled = true;
+            this.Race_Dropdown.Location = new System.Drawing.Point(251, 43);
+            this.Race_Dropdown.Name = "Race_Dropdown";
+            this.Race_Dropdown.Size = new System.Drawing.Size(192, 28);
+            this.Race_Dropdown.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(248, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Race";
+            // 
+            // Class_Dropdown
+            // 
+            this.Class_Dropdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Class_Dropdown.FormattingEnabled = true;
+            this.Class_Dropdown.Location = new System.Drawing.Point(13, 93);
+            this.Class_Dropdown.Name = "Class_Dropdown";
+            this.Class_Dropdown.Size = new System.Drawing.Size(192, 28);
+            this.Class_Dropdown.TabIndex = 12;
+            this.Class_Dropdown.SelectedValueChanged += new System.EventHandler(this.Class_Dropdown_SelectedValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(10, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Class";
+            // 
+            // Name_Textbox
+            // 
+            this.Name_Textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name_Textbox.Location = new System.Drawing.Point(13, 43);
+            this.Name_Textbox.Name = "Name_Textbox";
+            this.Name_Textbox.Size = new System.Drawing.Size(192, 27);
+            this.Name_Textbox.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(10, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Player Name ";
+            // 
+            // CreateGuildBox
+            // 
+            this.CreateGuildBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateGuildBox.Location = new System.Drawing.Point(12, 348);
+            this.CreateGuildBox.Name = "CreateGuildBox";
+            this.CreateGuildBox.Size = new System.Drawing.Size(633, 139);
+            this.CreateGuildBox.TabIndex = 9;
+            this.CreateGuildBox.TabStop = false;
+            this.CreateGuildBox.Text = "Create New Guild";
             // 
             // Form1
             // 
